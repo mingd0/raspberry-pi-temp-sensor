29c58fb65677a8333e3f1aab354e34cf6b84ca97
David Minge <minge.david@gmail.com>
2024-01-23 07:53:26 -0600

    Remove backend-archive

--- backend-archive/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import TemperatureReading
-
-
-class TemperatureReadingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TemperatureReading
-        fields = "__all__"  # Serialize all fields in the model
--- backend-archive/api/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import generics
-from .models import TemperatureReading
-from .serializers import TemperatureReadingSerializer
-
-
-class TemperatureReadingListCreate(generics.ListCreateAPIView):
-    queryset = TemperatureReading.objects.all()
-    serializer_class = TemperatureReadingSerializer
